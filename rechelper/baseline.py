7cf50de8a327116d76fac0a4240ca4f50199b8b5
Aki Saarinen <aki.saarinen@gmail.com>
2019-10-14 02:44:06 +0900

    Add Item+User mean

--- rechelper/baseline.py
+++ rechelper/baseline.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.sparse import coo_matrix
+from sklearn.metrics.pairwise import pairwise_distances
 
 class BaselineMean:
   def __init__(self, df_train, n_users, n_items):
@@ -7,11 +8,11 @@ class BaselineMean:
       (df_train['rating'], (df_train['user'], df_train['item'])),
       shape=(n_users, n_items)
     )
-
     # Calculate means
-    self.mean_item_ratings = coo.sum(axis=0).getA1() / np.bincount(coo.col, minlength=n_items)
-    self.mean_user_ratings = coo.sum(axis=1).getA1() / np.bincount(coo.row, minlength=n_users)
-    self.mean_all = np.nanmean(coo.data)
+    with np.errstate(invalid='ignore'):
+      self.mean_item_ratings = coo.sum(axis=0).getA1() / np.bincount(coo.col, minlength=n_items)
+      self.mean_user_ratings = coo.sum(axis=1).getA1() / np.bincount(coo.row, minlength=n_users)
+      self.mean_all = np.nanmean(coo.data)
 
     # Replace NaN with overall mean
     self.mean_item_ratings[np.isnan(self.mean_item_ratings)] = self.mean_all
